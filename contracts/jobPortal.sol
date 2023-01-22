b2d433117ac67e3b4dc12971f20e73714b8f1b03
misterpalmer <mandrew333@gmail.com>
2023-01-22 08:12:21 -0800

    chore: cleaned spacing, comments, and names

---
+++ contracts/jobPortal.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract jobPortal {
+    
+    /* Events */
+    event PositionEvent(string positionId, PositionStatus positionStatus, uint timeOfEvent);
+    
+    /* Enums / Structs */
+    enum SeekerEmploymentStatus {
+        SeekingEmployment,
+        Employed
+    }
+
+    enum PositionStatus {
+        Created,
+        Available,
+        Disabled,
+        Fulfilled
+    }
+
+    enum PositionSite {
+        FullRemote,
+        PartialRemote,
+        OnlyOnSite
+    }
+
+    enum PositionType {
+        Contract,
+        PartTime,
+        W2
+    }
+
+    struct Recruiter {
+        address id;
+        string companyName;
+        string companyLocation;
+        string recruiterName;
+        string website;
+        string recruiterID;
+    }
+
+    struct PositionSeeker {
+        address id;
+        string personalLocation;
+        string name;
+        uint256 phoneNumber;
+        string email;
+    }
+
+    struct Position {
+        string id;
+        address recuiter;
+        string title;
+        string description;
+        string requirements;
+        string[] skills;
+        PositionStatus status;
+        PositionSite site;
+        PositionType _type;
+    }
+
+    struct Candidate {
+        address seeker;
+        string positionId;
+        string appliedDate;
+    }
+
+    // todo validate the approach of mapping and keys[]
+    // https://hackmd.io/@totomanov/gas-optimization-loops
+    mapping (uint256 => Recruiter) public recruiters;
+    address[] recruiterKeys;
+
+    mapping (uint256 => PositionSeeker) public seekers;
+    uint256[] positionKeys;
+
+    mapping (uint256 => Position) public positions;
+    Position[] positionList; 
+
+    mapping (uint256 => Candidate) public candidates;
+
+/*
+    function registerJobSeeker (jobSeeker memory _jobSeeker, string memory name, uint index) public {
+        jobSeekers[index] = _jobSeeker;
+    }
+*/
+    function createPosition (
+        string memory id,
+        address recruiter,
+        string memory title,
+        string memory description,
+        string memory requirements,
+        string[] memory skills,
+        PositionSite positionSite,
+        PositionType positionType
+    ) public {
+        Position memory position = 
+            Position(id,
+                recruiter,
+                title,
+                description,
+                requirements,
+                skills,
+                PositionStatus.Created,
+                positionSite,
+                positionType
+            );
+        positionList.push(position); 
+        recruiterKeys.push(recruiter);
+        emit PositionEvent(id, PositionStatus.Created, block.timestamp);
+        // todo create an event in order to let the web3 clients to get noitifications when a job was created
+    }
+
+    function getPositions() public view returns(Position[] memory ) {
+        return positionList;
+    } 
+
+    function getAvailablePositions() public  view returns (Position[] memory ) {
+        Position[]  memory  availablePosition;
+        uint j=0;
+
+        for(uint i=0;i<positionList.length;i++)
+        {
+            if (positions[i].status==PositionStatus.Available){
+                 availablePosition[j]=(positions[i]);
+                 j++;
+            }
+        }
+        return availablePosition;
+    }
+
+    function changePositionType  (string memory  positionId,PositionStatus newStatus ) public{
+        // todo find the  position and modify the status
+        emit PositionEvent(positionId, newStatus, block.timestamp);
+    }
+
+    function getNumberOfPositions() public view  returns (uint){
+        return positionList.length;
+    }
+}
